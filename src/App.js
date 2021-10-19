488fc4698577beca87a516faeed99027961d9fa2
ayushicodes <raiayushi12911@gmail.com>
2021-10-19 21:37:20 +0530

    used class based component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Todo from './Todo';
 import Tododata from './Tododata';
 
-function App() {
-  const todolist = Tododata.map(item => <Todo key={item.id} Tododata={item} />)
-  return (
-    <div>
-      {todolist}
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      todos: Tododata
+    }
+  }
+
+  render() {
+    const todolist = this.state.todos.map(item => <Todo key={item.id} Tododata={item} />)
+    return (
+      <div>
+        {todolist}
+      </div>
+    )
+  }
 }
 
 export default App;
